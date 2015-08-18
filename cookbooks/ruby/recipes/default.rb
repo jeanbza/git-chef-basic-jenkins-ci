1b1ca8657fff1c0af628a16ee82fce7bbfe4eae0
Jean de Klerk <jadekler@gmail.com>
2015-08-17 22:24:27 -0600

    Fix echo; todo remove

--- cookbooks/ruby/recipes/default.rb
+++ cookbooks/ruby/recipes/default.rb
@@ -24,12 +24,13 @@ execute "set ruby global version to #{ruby_ver}" do
     command "rbenv global #{ruby_ver}"
 end
 
+# TODO: This may be unnecessary
 # execute 'open up rbenv directory for jenkins to use' do
 #     command 'chmod -R 777 /opt/rbenv'
 #     user 'root'
 # end
 
-# this may be unnecessary
+# TODO: remove bash profile appending
 execute 'add LD_LIBRARY_PATH to vagrant bash profile' do
-    command "echo 'export LD_LIBRARY_PATH=/usr/local/lib' >> /home/vagrant/.bash_profile"
+    command "echo 'export LD_LIBRARY_PATH=/usr/local/lib' >> /home/ec2-user/.bash_profile"
 end
